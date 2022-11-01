05e0a5ccd82d700acb28032bbb8ff8c69d20407f
rm86626 <106156564+rm86626@users.noreply.github.com>
2022-11-01 11:06:29 +0800

    App组件初始化-1

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
 import React,{Component} from 'react'
-import axios from 'axios'
-import Img1 from './img/1.gif'
-import Style from './css/style'
 
 class App extends Component{
-    render(){
-        return (
-            <div>
-                <h3 className={Style.special}>App00001组件</h3>
-                <span className={Style.one}>good</span>
-                <img src={Img1}/>
-                <ul>
-                {
-                    [
-                       <li key={1}>tom</li>, 
-                       <li key={2}>jarry</li>, 
-                       <li key={3}>susan</li> 
-                    ].map((value)=>{
-                        return value;
-                    })
-                }
-                </ul>
-                <div id='div1'>哈哈哈</div>
-            </div>
-        )
-    }
-    componentDidMount(){
-       axios.get("/data")
-       .then(res=>{
-        console.log("res",res);
-       });
-    }
+  constructor(){
+    console.log("constructor");
+    super();// 继承了父类Component
+           //this.props {}
+          //this.state {}
+  }
+  componentWillMount(){
+    console.log("componentWillMount 1");
+    this.sid=0;
+  }
+  render(){
+    console.log("render 2");
+    return (
+      // jsx->babel-loader->虚拟DOM
+      <div>
+         <h3 id="h">App</h3>
+       
+      </div>
+    )
+  }
+  // render调用完毕后，生成真实DOM
+  componentDidMount(){
+    {console.log(document.getElementById("h"))}
+    console.log("componentDidMount 3");
+    //操作真实DOM
+    //初始化组件ajax
+    //绘制图形库
+  }
 }
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom'
 import React from 'react'
 import App from './App'
 
+// ReactDOM React中用来操作DOM对象
 ReactDOM.render(
     <App/>,
     document.getElementById("app")
