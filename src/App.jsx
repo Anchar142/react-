dc02963f42cec859306de95ff20b065d2a03bbce
anchor <1421416463@qq.com>
2022-11-11 20:38:26 +0800

    axios练习post传参数

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,46 @@
+import axios from "axios";
 import classNames from "classnames";
 import React, { Component } from "react";
 import Style from "@/css/Style";
-import axios from "axios";
-import "@/mock/data.js";
-import "@/mock/data3.js";
+import "@/mock/data2.js";
 
 export default class App extends Component {
   state = {
-    stus1: [],
-    stus2: [],
+    stus: [],
     view: false,
   };
-  hasddleclick = () => {
-    this.setState({
-      stus1: [],
-      stus2: [],
-      view: true,
+  click = () => {
+    this.setState({ stus: [], view: true });
+    let url = new URLSearchParams();
+    url.append("name", "车车");
+    url.append("age", 11);
+    axios({
+      url: "data.php",
+      method: "post",
+      data: url,
+    }).then((res) => {
+      this.setState({
+        stus: res.data.data,
+        view: false,
+      });
     });
-    axios.patch([axios.get("data.php"), axios.get("data2.php")]).then(
-      axios.spread((res1, res2) => {
-        console.log(res1.data.data);
-        console.log(res2.data.data);
-        this.setState({
-          stus1: res1.data.data,
-          stus2: res2.data.data,
-          view: false,
-        });
-      })
-    );
   };
+
   render() {
-    let { stus1, stus2, view } = this.state;
+    let { stus, view } = this.state;
     let classes = classNames(Style.one, { [Style.load]: view });
     return (
       <div>
-        <h3>axios同时发送多个请求</h3>
-        <button onClick={this.hasddleclick}>点击发送请求</button>
+        <h3>aioxs post方法传送urlencoded数据</h3>
+        <button onClick={this.click}>点击发送请求</button>
         <ul className={classes}>
-          {stus1.map((value) => {
+          {stus.map((value) => {
             return (
               <li key={value.id}>
                 学号:{value.id}
                 姓名:{value.name}
-                性别:{value.age}
-                年龄:{value.sex}
-              </li>
-            );
-          })}
-        </ul>
-        <ul className={classes}>
-          {stus2.map((value) => {
-            return (
-              <li key={value.id}>
-                学号:{value.id}
-                姓名:{value.name}
-                性别:{value.age}
-                年龄:{value.sex}
+                性别：{value.sex}
+                年龄:{value.age}
               </li>
             );
           })}
